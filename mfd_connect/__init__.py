b2041a6f35a0db97c89fc5320e42b62939647dd6
Lasota, Adrian <adrian.lasota@intel.com>
2025-08-01 13:14:43 +0200

    fix: Fixed rpyc_server on ESXi
    On ESXi we cannot import requests, in WinRm it's imported, so moving it out from ESXi
    
    fixes #3
    
    Signed-off-by: Lasota, Adrian <adrian.lasota@intel.com>

--- mfd_connect/__init__.py
+++ mfd_connect/__init__.py
@@ -2,30 +2,26 @@
 # SPDX-License-Identifier: MIT
 """Package for Connection implementations."""
 
-import sys
 import logging
 
 logger = logging.getLogger(__name__)
 
-if "-m" in sys.argv and "mfd_connect.rpyc_server" in sys.argv:
-    logger.log(logging.DEBUG, "Running as a module - skipping importing in mfd_connect.__init__")
-else:
-    import platform
+import platform
 
-    from mfd_typing import OSName
+from mfd_typing import OSName
 
-    from .base import Connection, AsyncConnection, PythonConnection
-    from .local import LocalConnection
-    from .rpyc import RPyCConnection
-    from .tunneled_rpyc import TunneledRPyCConnection
-    from .sol import SolConnection
-    from .serial import SerialConnection
-    from .telnet.telnet import TelnetConnection
+from .base import Connection, AsyncConnection, PythonConnection
+from .local import LocalConnection
+from .rpyc import RPyCConnection
+from .tunneled_rpyc import TunneledRPyCConnection
+from .sol import SolConnection
+from .serial import SerialConnection
+from .telnet.telnet import TelnetConnection
+
+if platform.system() != OSName.ESXI.value:
     from .winrm import WinRmConnection
-
-    if platform.system() != OSName.ESXI.value:
-        from .ssh import SSHConnection
-        from .interactive_ssh import InteractiveSSHConnection
-        from .tunneled_ssh import TunneledSSHConnection
-        from .rpyc_zero_deploy import RPyCZeroDeployConnection
-        from .pxssh import PxsshConnection
+    from .ssh import SSHConnection
+    from .interactive_ssh import InteractiveSSHConnection
+    from .tunneled_ssh import TunneledSSHConnection
+    from .rpyc_zero_deploy import RPyCZeroDeployConnection
+    from .pxssh import PxsshConnection
